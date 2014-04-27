b2a638dfe3f7d62372655d7a5fd43a4eb3780cd4
Geoff Wild <geoff@IRVIGW-MAC.local>
2014-04-26 21:14:46 -0500

    adjusted tests to adhere to DRY

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let(:base_title) { "Northeast Wisconsin Hockey" }
+
 	describe "Home page" do
 
 		it "should have the content 'NEWHockey.com'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
 
 		it "should have the title 'Northeast Wisconsin Hockey | Home" do
 			visit '/static_pages/home'
-			expect(page).to have_title('Northeast Wisconsin Hockey | Home')
+			expect(page).to have_title("#{base_title} | Home")
 		end
 	end
 
@@ -23,7 +25,7 @@ describe "Static pages" do
 
 		it "should have the title 'Northeast Wisconsin Hockey | Help" do
 			visit '/static_pages/help'
-			expect(page).to have_title('Northeast Wisconsin Hockey | Help')
+			expect(page).to have_title("#{base_title} | Help")
 		end
 	end
 
@@ -35,7 +37,19 @@ describe "Static pages" do
 
 		it "should have the title 'Northeast Wisconsin Hockey | About" do
 			visit '/static_pages/about'
-			expect(page).to have_title('Northeast Wisconsin Hockey | About')
+			expect(page).to have_title("#{base_title} | About")
+		end
+	end
+
+	describe "Contact Page" do
+		it "should have the content 'Contact Us'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact Us')
+		end
+
+		it "should have the title 'Northeast Wisconsin Hockey | Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("#{base_title} | Contact")
 		end
 	end
 end
