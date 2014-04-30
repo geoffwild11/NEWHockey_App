944dd7427e57e62397fa29af437ef30749dc8654
Geoff Wild <geoff@IRVIGW-MAC.local>
2014-04-30 10:43:16 -0500

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	# Returns full title on per-page basis
+	def full_title(page_title)
+		base_title = "Northeast Wisconsin Hockey"
+		return base_title if page_title.empty?	# Returns just base title if empty
+		return "#{base_title} | #{page_title}"	# Returns full title if it isn't empty
+	end
+
 end
