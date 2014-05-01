718ea8a16bbd723390d0242ffda0df14fb7ac5a4
Geoff Wild <geoff@IRVIGW-MAC.local>
2014-05-01 14:47:47 -0500

    Done some HTML stuff

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,53 +7,53 @@ describe "Static pages" do
 	describe "Home page" do
 
 		it "should have the content 'NEWHockey.com'" do
-			visit '/static_pages/home'
+			visit root_path
 			expect(page).to have_content('NEWHockey.com')
 		end 
 
 		it "should have the title 'Northeast Wisconsin Hockey | Home" do
-			visit '/static_pages/home'
+			visit root_path
 			expect(page).to have_title("#{base_title}")
 		end
 
 		it "should not have a custom page title" do
-			visit '/static_pages/home'
+			visit root_path
 			expect(page).not_to have_title('| Home')
 		end
 	end
 
 	describe "Help page" do
 		it "should have the content 'NEWHockey Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_content('NEWHockey Help')
 		end
 
 		it "should have the title 'Northeast Wisconsin Hockey | Help" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_title("#{base_title} | Help")
 		end
 	end
 
 	describe "About page" do
 		it "should have the content 'About NEWHockey.com'" do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_content('About NEWHockey.com')
 		end
 
 		it "should have the title 'Northeast Wisconsin Hockey | About" do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_title("#{base_title} | About")
 		end
 	end
 
 	describe "Contact Page" do
 		it "should have the content 'Contact Us'" do
-			visit '/static_pages/contact'
+			visit contact_path
 			expect(page).to have_content('Contact Us')
 		end
 
 		it "should have the title 'Northeast Wisconsin Hockey | Contact'" do
-			visit '/static_pages/contact'
+			visit contact_path
 			expect(page).to have_title("#{base_title} | Contact")
 		end
 	end
