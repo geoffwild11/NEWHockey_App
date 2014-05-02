020d867255ba624edcf2a8fefd7feb4fbc98e0c7
Geoff Wild <geoff@IRVIGW-MAC.local>
2014-05-01 21:59:25 -0500

    RSpec looks nice

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,7 +9,7 @@ describe "Static pages" do
 		before { visit root_path }
  
 		it { should have_content('NEWHockey.com') }
-		it { should have_title("#{base_title}") }
+		it { should have_title(full_title('')) }
 		it { should_not have_title('| Home') }
 	end
 
@@ -17,20 +17,20 @@ describe "Static pages" do
 		before { visit help_path }
 
 		it { should have_content("NEWHockey Help") }
-		it { should have_title("#{base_title} | Help") }
+		it { should have_title(full_title('Help')) }
 	end
 
 	describe "About page" do
 		before { visit about_path }
 
 		it { should have_content("About NEWHockey.com") }
-		it { should have_title("#{base_title} | About") }
+		it { should have_title(full_title('About')) }
 	end
 
 	describe "Contact Page" do
 		before { visit contact_path }
 
 		it { should have_content("Contact Us") }
-		it { should have_title("#{base_title} | Contact")}
+		it { should have_title(full_title('Contact')) }
 	end
 end
