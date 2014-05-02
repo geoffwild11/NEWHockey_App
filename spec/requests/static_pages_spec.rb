8f44e8924dfab1e9f88553ea30e4b75e805ca0d1
Geoff Wild <geoff@IRVIGW-MAC.local>
2014-05-01 21:48:41 -0500

    Adjusted routes and tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,58 +3,34 @@ require 'spec_helper'
 describe "Static pages" do
 
 	let(:base_title) { "Northeast Wisconsin Hockey" }
+	subject { page }
 
 	describe "Home page" do
-
-		it "should have the content 'NEWHockey.com'" do
-			visit root_path
-			expect(page).to have_content('NEWHockey.com')
-		end 
-
-		it "should have the title 'Northeast Wisconsin Hockey | Home" do
-			visit root_path
-			expect(page).to have_title("#{base_title}")
-		end
-
-		it "should not have a custom page title" do
-			visit root_path
-			expect(page).not_to have_title('| Home')
-		end
+		before { visit root_path }
+ 
+		it { should have_content('NEWHockey.com') }
+		it { should have_title("#{base_title}") }
+		it { should_not have_title('| Home') }
 	end
 
 	describe "Help page" do
-		it "should have the content 'NEWHockey Help'" do
-			visit help_path
-			expect(page).to have_content('NEWHockey Help')
-		end
+		before { visit help_path }
 
-		it "should have the title 'Northeast Wisconsin Hockey | Help" do
-			visit help_path
-			expect(page).to have_title("#{base_title} | Help")
-		end
+		it { should have_content("NEWHockey Help") }
+		it { should have_title("#{base_title} | Help") }
 	end
 
 	describe "About page" do
-		it "should have the content 'About NEWHockey.com'" do
-			visit about_path
-			expect(page).to have_content('About NEWHockey.com')
-		end
+		before { visit about_path }
 
-		it "should have the title 'Northeast Wisconsin Hockey | About" do
-			visit about_path
-			expect(page).to have_title("#{base_title} | About")
-		end
+		it { should have_content("About NEWHockey.com") }
+		it { should have_title("#{base_title} | About") }
 	end
 
 	describe "Contact Page" do
-		it "should have the content 'Contact Us'" do
-			visit contact_path
-			expect(page).to have_content('Contact Us')
-		end
+		before { visit contact_path }
 
-		it "should have the title 'Northeast Wisconsin Hockey | Contact'" do
-			visit contact_path
-			expect(page).to have_title("#{base_title} | Contact")
-		end
+		it { should have_content("Contact Us") }
+		it { should have_title("#{base_title} | Contact")}
 	end
 end
